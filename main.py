53ca702ae1c2b476d5c3c03e8c75f1d01d09abd1
Atreyi Seal <atreyi.seal@digipen.edu>
2026-09-08 15:28:17 -0700

    Pseudocode for algorithm

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Takagi Sugeno Fuzzy System
+
+# if x is A(i) and y is B(k) then z = z(ik)
+
+# output is
+# summation (from (i = 1 to n; k = 1 to m)
+# (A(i)(x) max (B)(k)(x) * z(ik)))
+# divided by summation ((from (i = 1 to n; k = 1 to m))
+# (A(i)(x) max (B)(k)(x) * z(ik)))
+
 class Rule:
     def __init__(self, antecedents, consequent):
         self.antecedents = antecedents
