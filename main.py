9a752780ebb70293c7d2bcf8de481f125ee15966
Isaac Zhou <isaac.zhou.2004@gmail.com>
2026-09-09 09:44:06 -0700

    Added fuzzy sets for food quality and service.

--- main.py
+++ main.py
@@ -29,10 +29,39 @@ def food_quality_average(x):
 def food_quality_excellent(x):
     if 5 <= x <= 7.5:
         return (x - 5) / 2.5
-    elif 3 < x <= 5:
+    elif 7.5 < x <= 10:
+        return (5 - x) / 2
+    else:
+        return 0
+
+def service_poor(x):
+    if 0 <= x <= 2.5:
+        return x / 2.5
+    elif 2.5 < x <= 5:
+        return (5 - x) / 2.5
+    else:
+        return 0
+
+def service_average(x):
+    if 2.5 <= x <= 5:
+        return (x - 2.5)/ 2.5
+    elif 5 < x <= 7.5:
+        return (7.5 - x) / 2.5
+    else:
+        return 0
+
+def service_excellent(x):
+    if 5 <= x <= 7.5:
+        return (x - 5) / 2.5
+    elif 7.5 < x <= 10:
         return (5 - x) / 2
     else:
         return 0
 
 def main():
-    food_quality_low = fuzzy_logic.FuzzySet(food_quality_poor)
+    food_quality = fuzzy_logic.FuzzySet(
+        [food_quality_poor, food_quality_average, food_quality_excellent]
+    )
+    service = fuzzy_logic.FuzzySet(
+        [service_poor, service_average, service_excellent]
+    )
