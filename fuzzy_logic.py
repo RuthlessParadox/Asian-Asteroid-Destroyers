7395e62decf5392db63c72f2d6587d7c095cc036
Isaac Zhou <isaac.zhou.2004@gmail.com>
2026-09-09 11:50:35 -0700

    Added type hinting and corrected the Takagi-Sugeno calculation

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -7,20 +7,19 @@ class FuzzyRule(NamedTuple):
     fuzzy_sets: tuple[FuzzySet, ...]
     output: int | float
 
-class TNorm:
-    def __init__(self, function):
-        self.function = function
+type TNorm = Callable[[int | float, int | float], int | float]
 
-    def calculate(self, a, b):
-        return self.function(a, b)
-
-def takagi_sugeno_calculation(inputs, ruleset, t_norm):
+def takagi_sugeno_calculation(
+    inputs: tuple[int | float, ...],
+    ruleset: tuple[FuzzyRule, ...],
+    t_norm: TNorm,
+) -> int | float:
     total_outputs = 0.0
     total_firing_strengths = 0.0
     for rule in ruleset:
         firing_strength = 1
-        for index, fuzzy_set in rule.fuzzy_sets:
-            firing_strength = t_norm.calculate(firing_strength, inputs[index])
+        for index, fuzzy_set in enumerate(rule.fuzzy_sets):
+            firing_strength = t_norm(firing_strength, fuzzy_set(inputs[index]))
         total_outputs += firing_strength * rule.output
         total_firing_strengths += firing_strength
     return total_outputs / total_firing_strengths
